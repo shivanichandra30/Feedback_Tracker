146d37a1bcdcc260a39588c82df22f59d482060e
shivanichandra30 <166443725+shivanichandra30@users.noreply.github.com>
2025-05-27 12:39:46 +0530

    added configs

---
+++ Client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // Enable class based dark mode
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
